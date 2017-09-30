2fdeecc4e4c5d1e7c5fa65151b0b5e332dc9985b
SirYummy <micahbole.n@gmail.com>
2017-09-30 15:34:24 -0400

    Add config files.

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    __MarketPUBG_Environment__ : 'test',
+    __MarketPUBG_Author__ : 'SirYummy',
+    __MarketPUBG_Version__ : '1.0.0',
+    __MarketPUBG_API_Key__ : '03WmJQP+jtHW0A6iKWPuB5WKtL2D7K7OaeYx1+Q7ecB0vOqxSaXh/VHd11z7CGbI'
+}
